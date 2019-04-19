6b322284b852cdf008202fa0de1c83ac8c6fac05
Tri Nguyen <tri.seddon.nguyen@gmail.com>
2019-04-19 21:12:02 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -5,3 +5,10 @@ def display_board(board)
   puts "-----------"
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
+
+def valid_move?(board, index) 
+end
+
+def position_taken?(board, index)
+  return !(board[index] == " " || board[index] == "" || board[index] == nil)
+end
